be11746338dda982cbeb1efd9b0eb5b22c7e9566
Michelle Gower <mgower@illinois.edu>
2015-12-07 16:29:17 +0000

    Convert Makefile to setup.py.   Update table with commented out dependencies

---
+++ setup.py
@@ -0,0 +1,19 @@
+import distutils
+from distutils.core import setup
+import glob
+
+bin_files = glob.glob("bin/*")
+
+# The main call
+setup(name='IntegrationUtils',
+      version ='2.0.0',
+      license = "GPL",
+      description = "DESDM's integration utils",
+      author = "Michelle Gower",
+      author_email = "mgower@illinois.edu",
+      packages = ['intgutils'],
+      package_dir = {'': 'python'},
+      scripts = bin_files,
+      data_files=[('ups',['ups/IntegrationUtils.table'])]
+      )
+
