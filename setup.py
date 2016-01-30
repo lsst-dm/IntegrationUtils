741fa284e8c35a0fcd04119d5738812f8dfc9eb6
Gregory Daues <daues@illinois.edu>
2016-01-30 14:26:34 +0000

    fix version reporting with a new tag 2.0.3 on IntegrationUtils

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ bin_files = glob.glob("bin/*")
 
 # The main call
 setup(name='IntegrationUtils',
-      version ='2.0.0',
+      version ='2.0.3',
       license = "GPL",
       description = "DESDM's integration utils",
       author = "Michelle Gower",
