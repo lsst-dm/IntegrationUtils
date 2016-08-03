60f2ddf49b7118bdcbef1713487aa4d7db0be634
Felipe Menanteau <felipe@illinois.edu>
2016-08-03 20:54:45 +0000

    preparing for new tag

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ bin_files = glob.glob("bin/*")
 
 # The main call
 setup(name='IntegrationUtils',
-      version ='2.0.6',
+      version ='2.0.7',
       license = "GPL",
       description = "DESDM's integration utils",
       author = "Michelle Gower",
